4e488c9c4408473215eb98a38288275e1e496ccb
celineai <imanica@g.cofc.edu>
2021-09-01 11:59:36 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,17 @@
+"""
+name: Celine Imani
+hello.py
+
+problem: This program checks test my environment to make sure its running smoothly
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+
+"""
+
+
+def main():
+    print("hello, world!")
+
+
+main()
